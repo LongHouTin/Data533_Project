ed07146feccb47abfcc2a3e99d2a4b6ef42e7bf7
LongHouTin <lsam8910@gmail.com>
2019-12-09 18:16:41 -0800

    dum

--- testHSI.py
+++ testHSI.py
@@ -6,3 +6,4 @@ import testSam.testHSIinstant, testSam.testHSIhistoric
 
 HSIsuite=unittest.TestSuite()
 HSIsuite.addTests([testSam.testHSIinstant.TestHSIinstant("test_indices"),testSam.testHSIinstant.TestHSIinstant("test_stocks")])
+HSIsuite.addTests([testSam.testHSIhistoric.TestHSIhistoric("test_getprices"),testSam.testHSIhistoric.TestHSIhistoric("test_getreturns"),testSam.testHSIhistoric.TestHSIhistoric("test_getlogreturns"),testSam.testHSIhistoric.TestHSIhistoric("test_plotprices"),testSam.testHSIhistoric.TestHSIhistoric("test_plotreturns")])
