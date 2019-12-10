03f867c388b7c99bfba34f92a09be1cb9b361f08
LongHouTin <lsam8910@gmail.com>
2019-12-09 20:01:09 -0800

    dum

--- travisHSI.py
+++ travisHSI.py
@@ -5,6 +5,7 @@ from fed.crawl.crawl import webcrawl
 import fed.HSI.instant as it, fed.HSI.historic as ht
 import testSam.testHSIinstant, testSam.testHSIhistoric
 
+
 HSIsuite=unittest.TestSuite()
 HSIsuite.addTests([testSam.testHSIinstant.TestHSIinstant("test_indices"),testSam.testHSIinstant.TestHSIinstant("test_stocks")])
 HSIsuite.addTests([testSam.testHSIhistoric.TestHSIhistoric("test_getprices"),testSam.testHSIhistoric.TestHSIhistoric("test_getreturns"),testSam.testHSIhistoric.TestHSIhistoric("test_getlogreturns"),testSam.testHSIhistoric.TestHSIhistoric("test_plotprices"),testSam.testHSIhistoric.TestHSIhistoric("test_plotreturns")])
