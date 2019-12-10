2287872917427dcca6c5706cce479431c9c1ec8a
LongHouTin <lsam8910@gmail.com>
2019-12-09 18:04:51 -0800

    dum

--- testHSI.py
+++ testHSI.py
@@ -1,12 +1,2 @@
 import unittest
 from fed.crawl.crawl import webcrawl
-class TestCrawl(unittest.TestCase):
-    
-    def setUp(self):
-        print("this is setUp")
-        
-    def tearDown(self):
-        print("this is tearDown")
-    
-    def test_webcrawl(self):
-        self.assertEqual(webcrawl(),None)  # test should be carried out with internet connection otherwise failed
