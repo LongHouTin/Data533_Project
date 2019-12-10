a0804a7369aa9c269b48962f35709fb933393456
LongHouTin <lsam8910@gmail.com>
2019-12-09 18:03:38 -0800

    dum

--- testHSI.py
+++ testHSI.py
@@ -1,2 +1,12 @@
-from fed.calculate.fmath import netPurchaseChange
-
+import unittest
+from fed.crawl.crawl import webcrawl
+class TestCrawl(unittest.TestCase):
+    
+    def setUp(self):
+        print("this is setUp")
+        
+    def tearDown(self):
+        print("this is tearDown")
+    
+    def test_webcrawl(self):
+        self.assertEqual(webcrawl(),None)  # test should be carried out with internet connection otherwise failed
