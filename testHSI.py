fe9d82bfb3d18cd61562bebd5a9523ea7542e9fc
LongHouTin <lsam8910@gmail.com>
2019-12-09 17:52:53 -0800

    dum

--- testHSI.py
+++ testHSI.py
@@ -1,15 +1,13 @@
-"""
+
 import numpy as np, pandas as pd
 import fed.HSI.instant as it, fed.HSI.historic as ht
     
 import unittest
 import testSam.testHSIinstant, testSam.testHSIhistoric
 
-
+"""
 HSIsuite=unittest.TestSuite()
 HSIsuite.addTests([testSam.testHSIinstant.TestHSIinstant("test_indices"),testSam.testHSIinstant.TestHSIinstant("test_stocks")])
 HSIsuite.addTests([testSam.testHSIhistoric.TestHSIhistoric("test_getprices"),testSam.testHSIhistoric.TestHSIhistoric("test_getreturns"),testSam.testHSIhistoric.TestHSIhistoric("test_getlogreturns"),testSam.testHSIhistoric.TestHSIhistoric("test_plotprices"),testSam.testHSIhistoric.TestHSIhistoric("test_plotreturns")])
 unittest.TextTestRunner().run(HSIsuite)
 """
-
-pass
