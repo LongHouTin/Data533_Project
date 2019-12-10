4fa32dd57dabb82f19ef382986d29f107d1828a7
LongHouTin <lsam8910@gmail.com>
2019-12-09 18:12:57 -0800

    dum

--- testHSI.py
+++ testHSI.py
@@ -2,3 +2,4 @@ import unittest
 from fed.crawl.crawl import webcrawl
 import fed.HSI.instant as it, fed.HSI.historic as ht
 import testSam.testHSIinstant, testSam.testHSIhistoric
+# importing pandas and numpy throws errors, why ???
